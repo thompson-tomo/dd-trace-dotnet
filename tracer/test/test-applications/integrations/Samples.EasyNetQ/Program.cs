d3a60d2ba97c119635afb45cc392ae2112b89bab
Zach Montoya <zach.montoya@datadoghq.com>
2022-10-21 08:31:20 -0700

    Test multiple versions of the EasyNetQ NuGet package so we can confidentally declare support for it

--- tracer/test/test-applications/integrations/Samples.EasyNetQ/Program.cs
+++ tracer/test/test-applications/integrations/Samples.EasyNetQ/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,44 +26,47 @@ namespace Samples.EasyNetQ
 
         private static void RunEasyNetQ()
         {
-            var bus = RabbitHutch.CreateBus($"host={Host()}").Advanced;
-
-            // gotta get an IAdvancedBus from somewhere
-
-            var exchange = bus.ExchangeDeclare(exchangeName, "direct", durable: false, autoDelete: false);
-            var queue = bus.QueueDeclare(name: queueName, 
-                                            durable: false,
-                                            exclusive: false,
-                                            autoDelete: false);
-            bus.Bind(exchange, queue, routingKey);
-            bus.QueuePurge(queueName);
-
-            // Send message to the exchange
-            byte[] body = null;
-
-            bus.Publish(exchange, routingKey, mandatory: false, messageProperties: new(), body);
-            Console.WriteLine($"[Program.PublishAndGet] BasicPublish - Sent message: {string.Empty}");
-
-            // Send message to the default exchange and use new queue as the routingKey
-            string message = "PublishAndGetDefault - Message";
-            body = Encoding.UTF8.GetBytes(message);
-            bus.Publish(exchange, routingKey, mandatory: false, messageProperties: new(), body);
-            Console.WriteLine($"[Program.PublishAndGetDefault] BasicPublish - Sent message: {message}");
-
-            CountdownEvent cde = new(2);
-
-            var d = bus.Consume(queue, async (body, properties, info) =>
+            using (var bus = RabbitHutch.CreateBus($"host={Host()}").Advanced)
             {
-                await Task.Delay(100);
-                Console.WriteLine($"Consumed message with RoutingKey={info.RoutingKey} and MessageBody=\"{Encoding.UTF8.GetString(body.ToArray())}\"");
-                cde.Signal();
-            });
+                var exchange = bus.ExchangeDeclare(exchangeName, "direct", durable: false, autoDelete: false);
+                var queue = bus.QueueDeclare(name: queueName, 
+                                                durable: false,
+                                                exclusive: false,
+                                                autoDelete: false);
+                bus.Bind(exchange, queue, routingKey);
+#if EASYNETQ_7_0
+                bus.QueuePurge(queueName);
+#else
+                bus.QueuePurge(queue);
+#endif
 
-            // The countdown event will be triggered at the end of the Consume callback,
-            // before the automatic instrumentation's OnAsyncMethodEnd callback has been invoked.
-            // Add a short delay, in addition to the countdown event, to give the span a chance
-            // to close and send to the agent.
-            cde.Wait();
+                // Send message to the exchange
+                byte[] body = { };
+
+                bus.Publish(exchange, routingKey, mandatory: false, messageProperties: new(), body);
+                Console.WriteLine($"[Program.PublishAndGet] BasicPublish - Sent message: {string.Empty}");
+
+                // Send message to the default exchange and use new queue as the routingKey
+                string message = "PublishAndGetDefault - Message";
+                body = Encoding.UTF8.GetBytes(message);
+                bus.Publish(exchange, routingKey, mandatory: false, messageProperties: new(), body);
+                Console.WriteLine($"[Program.PublishAndGetDefault] BasicPublish - Sent message: {message}");
+
+                CountdownEvent cde = new(2);
+
+                using var d = bus.Consume(queue, async (body, properties, info) =>
+                {
+                    await Task.Delay(100);
+                    Console.WriteLine($"Consumed message with RoutingKey={info.RoutingKey} and MessageBody=\"{Encoding.UTF8.GetString(body.ToArray())}\"");
+                    cde.Signal();
+                });
+
+                // The countdown event will be triggered at the end of the Consume callback,
+                // before the automatic instrumentation's OnAsyncMethodEnd callback has been invoked.
+                // Add a short delay, in addition to the countdown event, to give the span a chance
+                // to close and send to the agent.
+                cde.Wait();
+            }
 
             Console.WriteLine("Waiting an additional 2 seconds to better ensure the consumer spans close.");
             Thread.Sleep(2000);
